d7574c1604ed5525ce330378b6591a4823b550b9
David A. Chentes <davidalejandrochentes@gmail.com>
2023-11-08 17:15:48 -0500

    dia2

--- software_empresarial/urls.py
+++ software_empresarial/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('mantenimiento/', include('mantenimiento.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
